b1ad7d2b06d8d2c1809b64b59d9cf236c557166c
Jean Mariette <jean.mariette@biwyze.fr>
2024-11-14 21:54:33 +0100

    Remove logging of request body in API class

--- lib/core/api.ts
+++ lib/core/api.ts
@@ -244,7 +244,6 @@ export class Api<T extends FTOptions> {
     }
 
     try {
-      console.log('body', body)
       await this.axiosInstance.post(this.endpoints.usages.track, stripUndefinedValues(body))
     }
     catch (err: any) {
